d40b41ca22fa8638c06cafa2e511714009b24c65
Tim Lentz <tlent234@live.kutztown.edu>
2022-03-24 21:11:40 -0400

    Update sideNavMealPlan.php
    fixed links so the wrap divs (<a><div></div><a>) and the whole square acts as a link
    changed <div> to <nav>

--- sideNavMealPlan.php
+++ sideNavMealPlan.php
@@ -1,36 +1,40 @@
-<div id = "left-nav-container">
-	<div id = "left-nav">
-		<div id= "left-nav-tile" style="padding: 2vh .5vw 2vh .5vw;">
-			<?php
-				$first = $_SESSION["firstname"];
-				echo "<a>Hello, $first</a>";
-			?>
-		</div>
-		<div id= "left-nav-tile" style="border-top: 2px solid white">
-			<a href=browse.php >Browse Recipes</a>
-		</div>
-		<div id= "left-nav-tile" style="border-bottom: .7px solid white; border-top: .7px solid white">
-			<a href=viewMeals.php >New Meal Plan</a>
-		</div>
-		<div id= "left-nav-tile" style="border-bottom: 2px solid white; pointer-events:none;">
-			<a>My Meal Plans</a>
-		</div>
-		<?php
-			$DB_HOST='localhost';
-			$DB_USER='fetcher1';
-			$DB_PASS='1234';
-			$DB_NAME='main'; 
-			$db = pg_connect("host={$DB_HOST} user={$DB_USER} password={$DB_PASS} dbname={$DB_NAME}");
-			
-			$userid = $_SESSION['userid'];
-			$res = pg_query($db, "SELECT * FROM meals WHERE customerid='$userid'");
-			while($row = pg_fetch_assoc($res)){
-				echo '<div id = "left-nav-tile"><a href="viewMeal.php?id=' . $row["mealid"] . '">' . $row['mealname'] . '</a></div>';
-			}
-			pg_close($db);
-		?>
-		<div id= "left-nav-tile" style="border-bottom: 2px solid white; border-top: 2px solid white">
-			<a>Grocery List</a>
-		</div>
-	</div>
-</div>
+<nav id = "left-nav">
+                <a class= "left-nav-tile" href=viewAccount.php>
+                    <?php
+                        $first = $_SESSION["firstname"];
+                        echo "<div  style='padding: 2vh .5vw 2vh .5vw;' >Hello, $first</div>";
+                    ?>
+                </a>
+                <a class= "left-nav-tile" href=browse.php >
+                    <div style="border-top: 2px solid white">
+                        Browse Recipes
+                    </div>
+                </a>
+                <a class= "left-nav-tile" href=viewMeals.php >
+                    <div  style="border-bottom: .7px solid white; border-top: .7px solid white">
+                        New Meal Plan
+                    </div>
+                </a>
+                <div class= "left-nav-tile" style="border-bottom: 2px solid white; pointer-events:none;">
+                    <a>My Meal Plans</a>
+                </div>
+                <?php
+                    $DB_HOST='localhost';
+                    $DB_USER='fetcher1';
+                    $DB_PASS='1234';
+                    $DB_NAME='main'; 
+                    $db = pg_connect("host={$DB_HOST} user={$DB_USER} password={$DB_PASS} dbname={$DB_NAME}");
+                    
+                    $userid = $_SESSION['userid'];
+                    $res = pg_query($db, "SELECT * FROM meals WHERE customerid='$userid'");
+                    while($row = pg_fetch_assoc($res)){
+                        echo '<a  class = "left-nav-tile" href="viewMeal.php?id=' . $row["mealid"] . '"><div>' . $row['mealname'] . '</div></a>';
+                    }
+                    //pg_close($db);
+                ?>
+                <div class= "left-nav-tile" style="border-bottom: 2px solid white; border-top: 2px solid white">
+                    <a>Grocery List</a>
+                </div>
+                
+</nav>
+<div id = "sidepad"></div>
